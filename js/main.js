ac69f54583fa245ed1e7fe1e275dea0785ab4bb7
Niclas Brandt <niclas.brandt@protonmail.com>
2023-11-02 09:00:17 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Get the current date
+const currentDate = new Date();
+currentDate.setUTCHours(00,00,00,00);
+
+// Get all the event elements (assuming they are in a list with a class name 'list-group')
+const eventElements = document.querySelectorAll('.list-group-item');
+
+// Iterate through the event elements
+eventElements.forEach((eventElement) => {
+    // Extract the date from the event element
+    const dateElement = eventElement.querySelector('.date');
+    const dateText = dateElement.textContent.trim();
+    const eventDate = new Date(dateText);
+
+    if (eventDate < currentDate) {
+        // The event has passed, so you can hide it (for example, using CSS)
+        eventElement.style.display = 'none';
+    }
+});
+
+
+selected = new Set();
+document.addEventListener("DOMContentLoaded", function () {
+    const selectableItems = document.querySelectorAll(".selectable");
+    selectableItems.forEach((item) => {
+        item.addEventListener("click", function () {
+            if (!item.classList.contains("text-success")) {
+                item.classList.add("text-success");
+                item.classList.remove("text-info");
+                selected.add(item.textContent);
+            } else {
+                item.classList.remove("text-success");
+                item.classList.add("text-info");
+                selected.delete(item.textContent);
+            }
+        });
+    });
+});
